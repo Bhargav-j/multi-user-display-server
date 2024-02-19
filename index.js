e60e9f6bf9d668f5dbe1fd8e37c5bfcef6fa208d
Bhargav-j <jeereddybhargav@gmail.com>
2024-02-19 11:18:27 +0530

    start command

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {WebSocketServer} = require("ws");
 const server = http.createServer();
 const wsServer = new WebSocketServer({server});
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const connections = {};
 const users = {};
